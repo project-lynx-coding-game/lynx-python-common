8fc79898be1e6db19ccffb8464e72d8e10e9d639
blazej-smorawski <blazej.smorawski@gmail.com>
2023-02-19 15:56:23 +0100

    Refactor `Serializable` class

--- lynx/common/serializable.py
+++ lynx/common/serializable.py
@@ -10,6 +10,27 @@ class Serializable:
         pass
 
     def __str__(self) -> str:
+        return self.serialize()
+
+    def serialize(self) -> str:
         exported_vars = {k: v for k, v in self.__dict__.items() if not k.startswith("_")}
-        return f"{{\"type\": \"{self.__class__.__name__}\", " \
-               f"\"data\": {json.dumps(exported_vars, default=str)}}}"
+        # The `lambda` converts strings such as `{}` into objects such as {},
+        # so we do not end up with nested strings with complex objects
+        return f"{json.dumps(exported_vars, default=lambda x: json.loads(x.serialize()))}"
+
+    def populate(self, json_string) -> None:
+        json_data = json.loads(json_string)
+        for exported_var in json_data.keys():
+            # If object we are deserializing is primitive
+            if not hasattr(self.__dict__[exported_var], '__dict__'):
+                self.__dict__[exported_var] = json_data[exported_var]
+            else:
+                variable_type = type(self.__dict__[exported_var])
+                self.__dict__[exported_var] = variable_type.deserialize(json.dumps(json_data[exported_var]))
+
+    @classmethod
+    def deserialize(cls, json_string: str): # TODO: returning `Self` was not working
+        # All serializable classes must have parameterless constructor
+        instance = cls()
+        instance.populate(json_string)
+        return instance
--- tests/serializable_test.py
+++ tests/serializable_test.py
@@ -1,15 +1,33 @@
+from dataclasses import dataclass
 import pytest
 import sys, os
 
 from lynx.common.serializable import Serializable
 
 def test_serializable():
+    @dataclass
+    class OtherTestClass(Serializable):
+        nested_field: str = "THIS_SHOULD_BE_NESTED"
+
     class TestClass(Serializable):
         def __init__(self) -> None:
             super().__init__()
             self.exported_field = "DO_EXPORT_THAT"
+            self.complex_field = OtherTestClass()
             self._private_field = "DO_!NOT!_EXPORT"
+            
+        def __eq__(self, __o: object) -> bool:
+            if not isinstance(__o, self.__class__):
+                return False
+            return (self.exported_field, self.complex_field) ==(__o.exported_field, __o.complex_field)
 
     test_object = TestClass()
-    log = str(test_object)
-    assert log == '{"type": "TestClass", "data": {"exported_field": "DO_EXPORT_THAT"}}'
+    test_object.exported_field += "+++"
+    test_object.complex_field.nested_field += "+++"
+    log = test_object.serialize()
+    assert log == '{"exported_field": "DO_EXPORT_THAT+++", "complex_field": {\"nested_field\": \"THIS_SHOULD_BE_NESTED+++\"}}'
+
+
+    deserialized = TestClass.deserialize(log)
+    assert deserialized == test_object
+
