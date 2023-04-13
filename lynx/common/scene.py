e5b3bc4d674aee3b34f45f65804d39160b3d154d
blazej-smorawski <blazej.smorawski@gmail.com>
2023-04-13 20:49:43 +0200

    Add `square` logic

--- lynx/common/scene.py
+++ lynx/common/scene.py
@@ -1,5 +1,6 @@
 from typing import Dict, NoReturn, Optional
 
+from lynx.common.square import Square
 from lynx.common.object import *
 from lynx.common.serializable import Serializable
 from lynx.common.vector import Vector
@@ -8,28 +9,38 @@ from lynx.common.vector import Vector
 @dataclass
 class Scene(Serializable):
     entities: List[Entity] = field(default_factory=list)
-    _object_position_map: Dict[Vector, Object] = field(default_factory=dict)
+    _square_position_map: Dict[Vector, Square] = field(default_factory=dict)
     _object_id_map: Dict[int, Object] = field(default_factory=dict)
+
+    def _get_square(self, position: Vector) -> Square:
+        # This method guarantees that the square is correct
+        if square := self._square_position_map.get(position):
+            return square
+
+        new_square = Square()
+        self._square_position_map[position] = new_square
+        return new_square
+
+    def _add_to_maps(self, entity: Entity) -> NoReturn:
+        if type(entity) is Object:
+            self._get_square(entity.position).append(entity)
+            self._object_id_map[entity.id] = entity
 
     def post_populate(self) -> NoReturn:
         for entity in self.entities:
-            if type(entity) is Object:
-                self._object_position_map[entity.position] = entity
-                self._object_id_map[entity.id] = entity
+            self._add_to_maps(entity)
 
     def add_entity(self, entity: Entity) -> NoReturn:
         self.entities.append(entity)
-        if type(entity) is Object:
-            self._object_position_map[entity.position] = entity
-            self._object_id_map[entity.id] = entity
+        self._add_to_maps(entity)
 
     def get_object_by_id(self, id: int) -> Optional[Object]:
         return self._object_id_map.get(id)
 
-    def get_object_by_position(self, position: Vector) -> Optional[Object]:
-        return self._object_map.get(position)
+    def get_objects_by_position(self, position: Vector) -> Optional[List[Object]]:
+        return self._get_square(position).objects
 
     def move_object(self, object: Object, vector: Vector) -> NoReturn:
-        self._object_position_map[object.position] = None
+        self._get_square(object.position).remove(object)
         object.position = object.position + vector
-        self._object_position_map[object.position] = object
+        self._get_square(object.position).append(object)
