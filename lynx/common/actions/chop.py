f29bc6d37affff9e321d0eab0c6ba55d240d7ee6
blazej-smorawski <blazej.smorawski@gmail.com>
2023-05-01 15:27:43 +0200

    Use upper-case names for `Objects`

--- lynx/common/actions/chop.py
+++ lynx/common/actions/chop.py
@@ -18,12 +18,12 @@ class Chop(Action):
 	def apply(self, scene: 'Scene') -> None:
 		objects_on_square = scene.get_objects_by_position(self.target_position)
 		for object in objects_on_square:
-			if object.name == 'tree':
+			if object.name == 'Tree':
 				remove_action = RemoveObject(object.id)
-				log = Object(id=scene.generate_id(), name='log', position=object.position, pickable=True)
+				log = Object(id=scene.generate_id(), name='Log', position=object.position, pickable=True)
 				create_action = CreateObject(log.serialize())
 				scene.add_to_pending_actions(remove_action.serialize())
 				scene.add_to_pending_actions(create_action.serialize())
 
 	def satisfies_requirements(self, scene: 'Scene') -> bool:
-		return CommonRequirements.is_in_range(scene, self.object_id, self.target_position, 1) and CommonRequirements.is_on_square(scene, self.target_position, 'tree')
+		return CommonRequirements.is_in_range(scene, self.object_id, self.target_position, 1) and CommonRequirements.is_on_square(scene, self.target_position, 'Tree')
