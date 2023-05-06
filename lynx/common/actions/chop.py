f36b6d5f5f92032f4cc3d11a245a67e85c0164c4
Krystian Jandy <s184589@student.pg.edu.pl>
2023-05-06 16:33:00 +0200

    LNX-126 Add displaying tree object

--- lynx/common/actions/chop.py
+++ lynx/common/actions/chop.py
@@ -1,30 +1,40 @@
 from dataclasses import dataclass
 
 from lynx.common.actions.action import Action
-from lynx.common.vector import Vector
-from lynx.common.actions.remove_object import RemoveObject
-from lynx.common.actions.create_object import CreateObject
-from lynx.common.object import Object
 from lynx.common.actions.common_requirements import CommonRequirements
+from lynx.common.actions.create_object import CreateObject
+from lynx.common.actions.remove_object import RemoveObject
+from lynx.common.object import Object
+from lynx.common.vector import Vector
+
 
 @dataclass
 class Chop(Action):
-	"""
-	Simple action used to hit/destroy object, which stands on the given destination.
-	"""
-	object_id: int = -1
-	target_position: Vector = Vector(1, 0)
+    """
+    Simple action used to hit/destroy object, which stands on the given destination.
+    """
+    object_id: int = -1
+    # target_position: Vector = Vector(1, 0) # Currently we want to chop the tree in specific direction
+    direction: Vector = Vector(1, 0)
 
-	def apply(self, scene: 'Scene') -> None:
-		objects_on_square = scene.get_objects_by_position(self.target_position)
-		for object in objects_on_square:
-			# TODO: put all object names into an Enum
-			if object.name == 'Tree':
-				remove_action = RemoveObject(object.id)
-				log = Object(id=scene.generate_id(), name='Log', position=object.position, pickable=True)
-				create_action = CreateObject(log.serialize())
-				scene.add_to_pending_actions(remove_action.serialize())
-				scene.add_to_pending_actions(create_action.serialize())
+    def apply(self, scene: 'Scene') -> None:
+        target_position = self._get_target_position(scene)
+        objects_on_square = scene.get_objects_by_position(target_position)
+        # TODO put 'Tree' into an Enum
+        chopped_object: Object = list(filter(lambda object_on_square: object_on_square.name == 'Tree', objects_on_square))[0]
+        if chopped_object:
+            remove_action = RemoveObject(chopped_object.id)
+            log = Object(id=scene.generate_id(), name='Log', pushable=True, walkable=False, position=chopped_object.position,
+                         pickable=True)
+            create_action = CreateObject(log.serialize())
+            scene.add_to_pending_actions(remove_action.serialize())
+            scene.add_to_pending_actions(create_action.serialize())
 
-	def satisfies_requirements(self, scene: 'Scene') -> bool:
-		return CommonRequirements.is_in_range(scene, self.object_id, self.target_position, 1) and CommonRequirements.is_on_square(scene, self.target_position, 'Tree')
+    def satisfies_requirements(self, scene: 'Scene') -> bool:
+        return CommonRequirements.is_in_range(scene, self.object_id, self._get_target_position(scene),
+                                              1) and CommonRequirements.is_on_square(scene,
+                                                                                     self._get_target_position(scene),
+                                                                                     'Tree')
+
+    def _get_target_position(self, scene: 'Scene') -> Vector:
+        return scene.get_object_by_id(self.object_id).position + self.direction
--- tests/chop_test.py
+++ tests/chop_test.py
@@ -1,65 +1,68 @@
-import unittest
+import random
 from typing import NoReturn
 
-from lynx.common.actions.chop import Chop
 from lynx.common.actions.action import Action
+from lynx.common.actions.chop import Chop
 from lynx.common.object import Object
 from lynx.common.scene import Scene
 from lynx.common.vector import Vector
-import random
+
 
 class TestChopSerialization:
-	expected_serialization_chop = '{"type": "Chop", "attributes": {"object_id": 1, "target_position": {"x": 2, "y": 2}}}'
+    expected_serialization_chop = '{"type": "Chop", "attributes": {"object_id": 1, "direction": {"x": 1, "y": 0}}}'
 
-	def test_success_serialization(self) -> NoReturn:
-		serialized_chop = Chop(object_id=1, target_position=Vector(2, 2)).serialize()
+    def test_success_serialization(self) -> NoReturn:
+        serialized_chop = Chop(object_id=1, direction=Vector(1, 0)).serialize()
 
-		assert self.expected_serialization_chop == serialized_chop
+        assert self.expected_serialization_chop == serialized_chop
 
 
 class TestChopApply:
-	def test_success_apply(self):
-		random.seed(1222)
-		expected_scene = Scene()
-		expected_dummy_object = Object(id=1, name="dummy", position=Vector(5, 5))
-		expected_dummy_chop = Chop(target_position=Vector(5, 6), object_id=1)
-		expected_dummy_object2 = Object(id=expected_scene.generate_id(), name="Log", position=Vector(5, 6), pickable = True)
-		expected_scene.add_entity(expected_dummy_object)
-		expected_scene.add_entity(expected_dummy_chop)
-		expected_scene.add_entity(expected_dummy_object2)
+    def test_success_apply(self):
+        random.seed(1222)
+        expected_scene = Scene()
+        expected_dummy_object = Object(id=1, name="dummy", position=Vector(5, 5))
+        expected_dummy_chop = Chop(direction=Vector(0, 1), object_id=1)
+        expected_dummy_object2 = Object(id=expected_scene.generate_id(), name="Log", position=Vector(5, 6),
+                                        pickable=True,  pushable=True, walkable=False)
+        expected_scene.add_entity(expected_dummy_object)
+        expected_scene.add_entity(expected_dummy_chop)
+        expected_scene.add_entity(expected_dummy_object2)
 
-		random.seed(1222)
-		scene = Scene()
-		dummy_object = Object(id=1, name="dummy", position=Vector(5, 5))
-		dummy_tree = Object(id=2, name="Tree", position=Vector(5, 6))
-		dummy_chop = Chop(target_position=Vector(5, 6), object_id=1)
-		scene.add_entity(dummy_object)
-		scene.add_entity(dummy_tree)
-		scene.add_entity(dummy_chop)
-		
-		if dummy_chop.satisfies_requirements(scene):
-			dummy_chop.apply(scene)
-		for action in scene.pending_actions:
-			Action.deserialize(action).apply(scene)
-		scene.pending_actions.clear()
+        random.seed(1222)
+        scene = Scene()
+        dummy_object = Object(id=1, name="dummy", position=Vector(5, 5))
+        dummy_tree = Object(id=2, name="Tree", position=Vector(5, 6))
+        dummy_chop = Chop(direction=Vector(0, 1), object_id=1)
+        scene.add_entity(dummy_object)
+        scene.add_entity(dummy_tree)
+        scene.add_entity(dummy_chop)
 
-		assert scene == expected_scene
+        if dummy_chop.satisfies_requirements(scene):
+            dummy_chop.apply(scene)
+        for action in scene.pending_actions:
+            Action.deserialize(action).apply(scene)
+        scene.pending_actions.clear()
+
+        assert scene == expected_scene
+
 
 class TestChopRequirements:
-	def test_success_requirements(self):
-		scene = Scene()
-		dummy_object = Object(id=1, name="dummy", position=Vector(5, 5))
-		dummy_tree = Object(id=2, name="Tree", position=Vector(5, 6))
-		dummy_chop = Chop(target_position=Vector(5, 6), object_id=1)
-		scene.add_entity(dummy_object)
-		scene.add_entity(dummy_tree)
-		assert dummy_chop.satisfies_requirements(scene) is True
 
-	def test_fail_requirements(self):
-		scene = Scene()
-		dummy_object = Object(id=1, name="dummy", position=Vector(5, 5))
-		dummy_tree = Object(id=2, name="Tree", position=Vector(5, 6))
-		dummy_chop = Chop(target_position=Vector(5, 3), object_id=1)
-		scene.add_entity(dummy_object)
-		scene.add_entity(dummy_tree)
-		assert dummy_chop.satisfies_requirements(scene) is not True
+    def test_success_requirements(self):
+        scene = Scene()
+        dummy_object = Object(id=1, name="dummy", position=Vector(5, 5))
+        dummy_tree = Object(id=2, name="Tree", position=Vector(5, 6))
+        dummy_chop = Chop(direction=Vector(0, 1), object_id=1)
+        scene.add_entity(dummy_object)
+        scene.add_entity(dummy_tree)
+        assert dummy_chop.satisfies_requirements(scene) is True
+
+    def test_fail_requirements(self):
+        scene = Scene()
+        dummy_object = Object(id=1, name="dummy", position=Vector(5, 5))
+        dummy_tree = Object(id=2, name="Tree", position=Vector(5, 6))
+        dummy_chop = Chop(direction=Vector(1, 0), object_id=1)
+        scene.add_entity(dummy_object)
+        scene.add_entity(dummy_tree)
+        assert dummy_chop.satisfies_requirements(scene) is not True
