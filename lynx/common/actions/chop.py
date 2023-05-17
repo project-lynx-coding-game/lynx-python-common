1f3c5530f4e7861a43e00cad7bfa732900288416
EasyJBL <kubaprofesor@gmail.com>
2023-05-17 16:47:57 +0200

    LNX-147 Removed tabs :)

--- lynx/common/actions/chop.py
+++ lynx/common/actions/chop.py
@@ -11,30 +11,30 @@ from lynx.common.vector import Vector
 
 @dataclass
 class Chop(Action):
-	"""
-	Simple action used to hit/destroy object, which stands on the given destination.
-	"""
-	object_id: int = -1
-	# target_position: Vector = Vector(1, 0) # Currently we want to chop the tree in specific direction
-	direction: Vector = Direction.NORTH.value
+    """
+    Simple action used to hit/destroy object, which stands on the given destination.
+    """
+    object_id: int = -1
+    # target_position: Vector = Vector(1, 0) # Currently we want to chop the tree in specific direction
+    direction: Vector = Direction.NORTH.value
 
-	def apply(self, scene: 'Scene') -> None:
-		target_position = self._get_target_position(scene)
-		objects_on_square = scene.get_objects_by_position(target_position)
-		# TODO put 'Tree' into an Enum
-		chopped_object: Object = \
-			list(filter(lambda object_on_square: object_on_square.name == 'Tree', objects_on_square))[0]
-		if chopped_object:
-			remove_action = RemoveObject(chopped_object.id)
-			log = Object(id=scene.generate_id(), name='Log', tags=['pushable', 'walkable', 'pickable'],
-						 position=chopped_object.position)
-			create_action = CreateObject(log.serialize())
-			scene.add_to_pending_actions(remove_action.serialize())
-			scene.add_to_pending_actions(create_action.serialize())
+    def apply(self, scene: 'Scene') -> None:
+        target_position = self._get_target_position(scene)
+        objects_on_square = scene.get_objects_by_position(target_position)
+        # TODO put 'Tree' into an Enum
+        chopped_object: Object = \
+            list(filter(lambda object_on_square: object_on_square.name == 'Tree', objects_on_square))[0]
+        if chopped_object:
+            remove_action = RemoveObject(chopped_object.id)
+            log = Object(id=scene.generate_id(), name='Log', tags=['pushable', 'walkable', 'pickable'],
+                         position=chopped_object.position)
+            create_action = CreateObject(log.serialize())
+            scene.add_to_pending_actions(remove_action.serialize())
+            scene.add_to_pending_actions(create_action.serialize())
 
-	def satisfies_requirements(self, scene: 'Scene') -> bool:
-		return CommonRequirements.is_in_range(scene, self.object_id, self._get_target_position(scene), 1) \
-			   and CommonRequirements.is_on_square(scene, self._get_target_position(scene), 'Tree')
+    def satisfies_requirements(self, scene: 'Scene') -> bool:
+        return CommonRequirements.is_in_range(scene, self.object_id, self._get_target_position(scene), 1) \
+               and CommonRequirements.is_on_square(scene, self._get_target_position(scene), 'Tree')
 
-	def _get_target_position(self, scene: 'Scene') -> Vector:
-		return scene.get_object_by_id(self.object_id).position + self.direction
+    def _get_target_position(self, scene: 'Scene') -> Vector:
+        return scene.get_object_by_id(self.object_id).position + self.direction
--- lynx/common/actions/common_requirements.py
+++ lynx/common/actions/common_requirements.py
@@ -7,52 +7,52 @@ from typing import List
 
 class CommonRequirements:
 
-	@staticmethod
-	def is_walkable(scene: 'Scene', position: Vector) -> bool:
-		square_at_destination: Square = scene.get_square(position)
-		return square_at_destination.walkable()
+    @staticmethod
+    def is_walkable(scene: 'Scene', position: Vector) -> bool:
+        square_at_destination: Square = scene.get_square(position)
+        return square_at_destination.walkable()
 
-	@staticmethod
-	def is_on_square(scene: 'Scene', position: Vector, name: str) -> bool:
-		objects_on_square = scene.get_objects_by_position(position)
-		return name in [object.name for object in objects_on_square]
+    @staticmethod
+    def is_on_square(scene: 'Scene', position: Vector, name: str) -> bool:
+        objects_on_square = scene.get_objects_by_position(position)
+        return name in [object.name for object in objects_on_square]
 
-	@staticmethod
-	def is_in_range(scene: 'Scene', object_id: int, position: Vector, max_distance: int) -> bool:
-		object: Object = scene.get_object_by_id(object_id)
+    @staticmethod
+    def is_in_range(scene: 'Scene', object_id: int, position: Vector, max_distance: int) -> bool:
+        object: Object = scene.get_object_by_id(object_id)
 
-		if object is None:
-			return False
+        if object is None:
+            return False
 
-		distance = object.position.dist_to(position)
-		return distance <= max_distance
+        distance = object.position.dist_to(position)
+        return distance <= max_distance
 
-	# object can be stacked on a given position if an object with the same name is already there
-	@staticmethod
-	def can_be_stacked(scene: 'Scene', position: Vector, object_name: str) -> bool:
-		objects_on_square = scene.get_objects_by_position(position)
+    # object can be stacked on a given position if an object with the same name is already there
+    @staticmethod
+    def can_be_stacked(scene: 'Scene', position: Vector, object_name: str) -> bool:
+        objects_on_square = scene.get_objects_by_position(position)
 
-		for object in objects_on_square:
-			if object.name == object_name:
-				return True
+        for object in objects_on_square:
+            if object.name == object_name:
+                return True
 
-		return False
+        return False
 
-	# TODO: Move, Push, Chop etc. should take argument of Direction type, not Vector type
-	# this means that serialization and deserialization have to be adapted to Enum
-	# when this is done, there is no need for this requirement
-	@staticmethod
-	def is_proper_direction(vector: Vector) -> bool:
-		direction_vectors = [Direction.NORTH.value, Direction.SOUTH.value, Direction.EAST.value, Direction.WEST.value]
-		return vector in direction_vectors
+    # TODO: Move, Push, Chop etc. should take argument of Direction type, not Vector type
+    # this means that serialization and deserialization have to be adapted to Enum
+    # when this is done, there is no need for this requirement
+    @staticmethod
+    def is_proper_direction(vector: Vector) -> bool:
+        direction_vectors = [Direction.NORTH.value, Direction.SOUTH.value, Direction.EAST.value, Direction.WEST.value]
+        return vector in direction_vectors
 
-	@staticmethod
-	def has_proper_tags(scene: 'Scene', object_id: int, tag_names: List[str]) -> bool:
-		object: Object = scene.get_object_by_id(object_id)
-		if len(tag_names) > len(object.tags):
-			return False
+    @staticmethod
+    def has_proper_tags(scene: 'Scene', object_id: int, tag_names: List[str]) -> bool:
+        object: Object = scene.get_object_by_id(object_id)
+        if len(tag_names) > len(object.tags):
+            return False
 
-		if all(element in object.tags for element in tag_names):
-			return True
+        if all(element in object.tags for element in tag_names):
+            return True
 
-		return False
+        return False
