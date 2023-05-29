59b35ad2a91d0b42f336a87caab8c67f4506e300
blazej-smorawski <56126596+blazej-smorawski@users.noreply.github.com>
2023-05-29 19:43:30 +0200

    Add `ErrorLog` (#16)

---
+++ lynx/common/actions/error_log.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from lynx.common.actions.action import Action
+
+
+@dataclass
+class ErrorLog(Action):
+    """
+    Simple action for logging errors during runtime
+    """
+    text: str = ""
+
+    def satisfies_requirements(self, scene: 'Scene') -> bool:
+        return True
+
+    def apply(self, scene: 'Scene') -> None:
+        pass
