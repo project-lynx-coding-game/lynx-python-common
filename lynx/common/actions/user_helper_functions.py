8ce98d227dc7bef4b5ad56082ef65aae4743f6ec
Jakub Jabłoński <99693341+EasyJB@users.noreply.github.com>
2023-08-08 21:00:08 +0200

    LNX-177 Add simple methods to allow, moving around map (#23)
    * LNX-177 Add simple methods to allow, moving around map
    
    * LNX-177 Added tests and some adjustments
    
    * fixed typo
    
    * refactor: minor refactor
    
    * Remove unused annotations import
    
    * Simplify tests
    
    * Improve bool asserts
    
    ---------
    
    Co-authored-by: Krystian Jandy <s184589@student.pg.edu.pl>
    Co-authored-by: FilipSzweda <filip.szweda1@gmail.com>

---
+++ lynx/common/actions/user_helper_functions.py
@@ -0,0 +1,118 @@
+import random
+from typing import List
+
+from lynx.common.actions.common_requirements import CommonRequirements as Req
+from lynx.common.vector import Vector
+
+
+def get_objects_around(object_id: int, scene: 'Scene', distance: int) -> List[int]:
+    """Gets the ids of the objects around the object.
+
+    Args:
+        object_id: The id of the object.
+        scene: The scene.
+        distance: The distance from the object.
+
+    Returns:
+        The ids of the objects around the object.
+
+    """
+    selected_objects_ids: List[int] = []
+    agent_position: Vector = scene.get_object_by_id(object_id).position
+    for obj in scene.entities:
+        if obj.position.manhattan_distance(agent_position) <= distance:
+            selected_objects_ids.append(obj.id)
+    selected_objects_ids.remove(object_id)
+    return selected_objects_ids
+
+
+def get_position(scene: 'Scene', object_id: int) -> Vector:
+    """Returns the position of the object.
+
+    Args:
+        scene: The scene.
+        object_id: The id of the object.
+
+    Returns:
+        The position of the object.
+
+    """
+    return scene.get_object_by_id(object_id).position
+
+
+def can_i_stand(scene: 'Scene', position: Vector) -> bool:
+    """Returns True if the position is walkable.
+
+    Args:
+        scene: The scene.
+        position: The position.
+
+    Returns:
+        True if the position is walkable.
+
+    """
+    return Req.is_walkable(scene, position)
+
+
+def get_type(scene: 'Scene', object_id: int) -> str:
+    """Returns the type of the object.
+
+    Args:
+        scene: The scene.
+        object_id: The id of the object.
+
+    Returns:
+        The type of the object.
+
+    """
+    return scene.get_object_by_id(object_id).name
+
+
+def calculate_distance_to(scene: 'Scene', object_id: int, agent_id: int) -> int:
+    """Returns the Manhattan distance between two objects.
+
+    Args:
+        scene: The scene.
+        object_id: The id of the first object.
+        agent_id: The id of the second object.
+
+    Returns:
+        The Manhattan distance between two objects.
+    """
+    return scene.get_object_by_id(agent_id).position.manhattan_distance(scene.get_object_by_id(object_id).position)
+
+
+def filter_objects(scene: 'Scene', object_ids: List[int], object_type: str) -> List[int]:
+    """Returns a list of object ids of the given type.
+
+    Args:
+        scene: The scene.
+        object_ids: The list of object ids to filter.
+        object_type: The type of the objects to filter.
+
+    Returns:
+        The list of object ids of the given type.
+
+    """
+    filtered_objects_ids: List[int] = list(
+        filter(lambda object_id: scene.get_object_by_id(object_id).name == object_type, object_ids)
+    )
+    return filtered_objects_ids
+
+
+def random_direction(scene: 'Scene', object_id: int) -> Vector:
+    """Returns a random direction from the list of available directions.
+
+    Args:
+        scene: The scene.
+        object_id: The id of the object.
+
+    Returns:
+        A random direction from the list of available directions.
+    """
+    positions: List[Vector] = [Vector(0, 1), Vector(1, 0), Vector(-1, 0), Vector(0, -1)]
+    object_position: Vector = scene.get_object_by_id(object_id).position
+    available_positions = list(
+        filter(lambda position: Req.is_walkable(scene, object_position + position), positions)
+    )
+    return random.choice(available_positions)
---
+++ tests/user_helper_functions_test.py
@@ -0,0 +1,58 @@
+import random
+from typing import NoReturn
+
+from lynx.common.object import Object
+from lynx.common.scene import Scene
+from lynx.common.vector import Vector
+from lynx.common.actions.user_helper_functions import get_objects_around, get_position, can_i_stand, get_type, calculate_distance_to, filter_objects, random_direction
+
+
+class TestUserHelperFunctions:
+    scene = Scene()
+    dummy_agent = Object(id=1, name="agent", position=Vector(0, 0))
+    scene.add_entity(dummy_agent)
+    dummy_object1 = Object(id=2, name="dummy", position=Vector(5, 5), tags=["walkable"])
+    scene.add_entity(dummy_object1)
+    dummy_object2 = Object(id=3, name="dummy", position=Vector(3, 5))
+    scene.add_entity(dummy_object2)
+    dummy_object3 = Object(id=4, name="diff_dummy", position=Vector(1, 1))
+    scene.add_entity(dummy_object3)
+    random.seed(12)
+
+    def test_objects_around_success(self) -> NoReturn:
+        assert get_objects_around(1, self.scene, 9) == [3, 4]
+
+    def test_objects_around_failure(self) -> NoReturn:
+        assert get_objects_around(1, self.scene, 7) != [3, 4]
+
+    def test_get_position_success(self) -> NoReturn:
+        assert get_position(self.scene, 3) == Vector(3, 5)
+
+    def test_can_i_stand_success(self) -> NoReturn:
+        assert can_i_stand(self.scene, Vector(5, 5)) is True
+
+    def test_can_i_stand_failure(self) -> NoReturn:
+        assert can_i_stand(self.scene, Vector(3, 5)) is False
+
+    def test_get_type_success(self) -> NoReturn:
+        assert get_type(self.scene, 1) == "agent"
+
+    def test_get_type_failure(self) -> NoReturn:
+        assert get_type(self.scene, 2) != "agent"
+
+    def test_distance_to_success(self) -> NoReturn:
+        assert calculate_distance_to(self.scene, 4, 1) == 2
+
+    def test_filter_objects_success(self) -> NoReturn:
+        assert filter_objects(self.scene, [1, 2, 3, 4], "dummy") == [2, 3]
+
+    def test_filter_objects_failure(self) -> NoReturn:
+        assert filter_objects(self.scene, [1, 2, 3, 4], "empty") != [1]
+
+    def test_random_direction_success(self) -> NoReturn:
+        self.scene.add_entity(Object(id=10, name="Grass", position=Vector(-1, 0), tags=["walkable"]))
+        self.scene.add_entity(Object(id=11, name="Grass", position=Vector(0, 1), tags=["walkable"]))
+        assert random_direction(self.scene, 1) == Vector(-1, 0)
+
+    def test_random_direction_failure(self) -> NoReturn:
+        assert random_direction(self.scene, 1) != Vector(1, 0)
