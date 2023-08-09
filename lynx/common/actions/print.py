7adfb3e242764b60f5f1de5a70de5b775d9cb581
Krystian Jandy <s184589@student.pg.edu.pl>
2023-08-09 17:56:19 +0200

    feat: add print builtin

---
+++ lynx/common/actions/print.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from lynx.common.actions.action import Action
+
+
+@dataclass
+class Print(Action):
+    """
+    Simple action for printing player's message
+    """
+    text: str = ""
+
+    def satisfies_requirements(self, scene: 'Scene') -> bool:
+        return True
+
+    def apply(self, scene: 'Scene') -> None:
+        pass
